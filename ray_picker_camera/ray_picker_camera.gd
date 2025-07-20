e5e4700a27a85a8bcf9e7bc6e441b74e006981b3
yubinex <janhauck99@gmail.com>
2025-07-20 15:03:39 +0200

    feat: Implement grid interaction on mouse hover
    Enables the RayPickerCamera to interact with the GridMap based on the mouse's position.
    
    - The camera's raycast continuously detects the grid cell under the mouse cursor.
    - When the cursor hovers over a FREE_SPACE tile, it is automatically converted to a TURRET_SPACE.

--- ray_picker_camera/ray_picker_camera.gd
+++ ray_picker_camera/ray_picker_camera.gd
@@ -2,12 +2,28 @@ extends Camera3D
 
 
 @export var raycast_distance: float = 100.0
+@export var grid_map: GridMap
 
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+
+enum TileType {
+	FREE_SPACE = 0,
+	TURRET_SPACE = 1,
+}
 
 
 func _process(_delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * raycast_distance
 	ray_cast_3d.force_raycast_update()
-	printt(ray_cast_3d.get_collider(), ray_cast_3d.get_collision_point())
+
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+
+		if collider is GridMap:
+			var collision_point: Vector3 = ray_cast_3d.get_collision_point()
+			var cell_coordinates: Vector3i = grid_map.local_to_map(collision_point)
+
+			if grid_map.get_cell_item(cell_coordinates) == TileType.FREE_SPACE:
+				grid_map.set_cell_item(cell_coordinates, TileType.TURRET_SPACE)
