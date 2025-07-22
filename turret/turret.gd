0a8a88966db8a7310f409a5ed764b4c42c193f05
yubinex <janhauck99@gmail.com>
2025-07-22 08:18:03 +0200

    feat: Implement automatic turret firing
    Adds a Timer to the turret scene to enable automatic firing.
    
    - Connects the Timer's timeout signal to a new _on_timer_timeout method in turret.gd.
    - This method instantiates and positions the projectile at the turret's top, creating a periodic firing mechanism.
    - The autostart property on the Timer ensures the turret begins firing as soon as it's in the scene.

--- turret/turret.gd
+++ turret/turret.gd
@@ -3,7 +3,10 @@ extends Node3D
 
 @export var projectile: PackedScene
 
+@onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
 
-func _ready() -> void:
+
+func _on_timer_timeout() -> void:
 	var shot: Projectile = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = turret_top.global_position
