40574bbed09c2af9f9e7c0da39ddd7f74ed20f81
yubinex <janhauck99@gmail.com>
2025-07-23 17:37:48 +0200

    feat(turret): Implement smarter targeting within range
    - Add a turret_range variable to define the turret's attack radius.
    
    - Implement a find_best_target function to select the enemy that has progressed the furthest along the path within the turret's range.
    
    - The turret now only aims and fires when a valid target is acquired.

--- turret/turret.gd
+++ turret/turret.gd
@@ -3,19 +3,40 @@ extends Node3D
 
 
 @export var projectile: PackedScene
+@export var turret_range := 10.0
 
 var enemy_path: Path3D
+var target: PathFollow3D
 
 @onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
 
 
 func _physics_process(_delta: float) -> void:
-	var enemy = enemy_path.get_children().back()
-	look_at(enemy.global_position, Vector3.UP, true)
+	target = find_best_target()
+	if target: look_at(target.global_position, Vector3.UP, true)
 
 
 func _on_timer_timeout() -> void:
+	if not target: return
+
 	var shot: Projectile = projectile.instantiate()
 	add_child(shot)
 	shot.global_position = turret_top.global_position
 	shot.direction = global_transform.basis.z
+
+
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var furthest_progress := 0
+	for enemy in enemy_path.get_children():
+		if enemy is not PathFollow3D:
+			continue
+
+		var distance := global_position.distance_to(enemy.global_position)
+		if distance > turret_range:
+			continue
+
+		if enemy.progress > furthest_progress:
+			best_target = enemy
+			furthest_progress = enemy.progress
+	return best_target
