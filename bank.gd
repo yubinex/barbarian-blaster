92cfb111d483d6404f8424da591f1e4f222540cc
yubinex <janhauck99@gmail.com>
2025-07-25 18:04:35 +0200

    feat: Add gold display UI component
    - Added Bank node with Label to display gold in the UI
    - Created bank.gd script to manage gold value and label updates
    - Included UID file for script resource management and LabelSettings

---
+++ bank.gd
@@ -0,0 +1,16 @@
+extends MarginContainer
+
+
+@export var starting_gold := 150
+
+var gold: int:
+	set(gold_in):
+		gold = max(gold_in, 0)
+
+		label.text = "gold: " + str(gold)
+
+@onready var label: Label = $Label
+
+
+func _ready() -> void:
+	gold = starting_gold
