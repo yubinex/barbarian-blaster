73376c7bceb4e2ef8d1b822d7fc43d48e0cf3364
yubinex <janhauck99@gmail.com>
2025-07-26 01:17:52 +0200

    feat: Implemented a gold and economy system for the game
    This commit introduces a new gold and economy system, which is essential for managing resources and player progression in the game. The key changes include:
    
    - **Gold for defeating enemies**: Enemies now grant a specific amount of gold upon being defeated, which provides players with a rewarding feedback loop and a primary way to earn resources.
    
    - **Turret construction cost**: Building a turret now costs 100 gold, adding a strategic layer to the game where players must manage their resources wisely.
    
    - **Centralized bank for gold management**: A new "Bank" node has been added to the `base_level.tscn` and `project.godot` files, which centralizes the management of the players gold and makes it easier to track and update.

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -4,6 +4,7 @@ extends PathFollow3D
 
 @export var speed: float = 5
 @export var max_health := 50
+@export var gold_value := 15
 
 var current_health: int:
 	set(health_in):
@@ -13,9 +14,11 @@ var current_health: int:
 		current_health = health_in
 
 		if current_health < 1:
+			bank.gold += gold_value
 			queue_free()
 
 @onready var base: Node3D = get_tree().get_first_node_in_group("base")
+@onready var bank = get_tree().get_first_node_in_group("bank")
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
@@ -23,7 +26,6 @@ func _ready() -> void:
 	current_health = max_health
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress += delta * speed
 	if progress_ratio == 1.0:
--- ray_picker_camera/ray_picker_camera.gd
+++ ray_picker_camera/ray_picker_camera.gd
@@ -4,8 +4,10 @@ extends Camera3D
 @export var raycast_distance: float = 100.0
 @export var grid_map: GridMap
 @export var turret_manager: TurretManager
+@export var turret_cost := 100
 
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
+@onready var bank = get_tree().get_first_node_in_group("bank")
 
 
 enum TileType {
@@ -20,17 +22,19 @@ func _process(_delta: float) -> void:
 	ray_cast_3d.force_raycast_update()
 
 	if ray_cast_3d.is_colliding():
-		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
-		var collider = ray_cast_3d.get_collider()
+		if bank.gold >= turret_cost:
+			Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+			var collider = ray_cast_3d.get_collider()
 
-		if collider is GridMap:
-			if Input.is_action_just_pressed("click"):
-				var collision_point: Vector3 = ray_cast_3d.get_collision_point()
-				var cell_coordinates: Vector3i = grid_map.local_to_map(collision_point)
+			if collider is GridMap:
+				if Input.is_action_just_pressed("click"):
+					var collision_point: Vector3 = ray_cast_3d.get_collision_point()
+					var cell_coordinates: Vector3i = grid_map.local_to_map(collision_point)
 
-				if grid_map.get_cell_item(cell_coordinates) == TileType.FREE_SPACE:
-					grid_map.set_cell_item(cell_coordinates, TileType.TURRET_SPACE)
-					var tile_position: Vector3 = grid_map.map_to_local(cell_coordinates)
-					turret_manager.build_turret(tile_position)
-	else:
-		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+					if grid_map.get_cell_item(cell_coordinates) == TileType.FREE_SPACE:
+						grid_map.set_cell_item(cell_coordinates, TileType.TURRET_SPACE)
+						var tile_position: Vector3 = grid_map.map_to_local(cell_coordinates)
+						turret_manager.build_turret(tile_position)
+						bank.gold -= turret_cost
+		else: Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+	else: Input.set_default_cursor_shape(Input.CURSOR_ARROW)
