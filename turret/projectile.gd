c7830265081867d9dd8738ceb1df79826f640e97
yubinex <janhauck99@gmail.com>
2025-07-29 19:02:47 +0200

    feat: Balance gameplay through difficulty and projectile changes
    Adjusted DifficultyManager curves for spawn time and enemy health to create a more progressive challenge. The game length has been set to 180 seconds.
    
    Projectile damage is now an exported variable, allowing for easier tuning and balancing.

--- turret/projectile.gd
+++ turret/projectile.gd
@@ -3,6 +3,7 @@ extends Area3D
 
 
 @export var speed := 30.0
+@export var damage := 25
 
 var direction := Vector3.FORWARD
 
@@ -17,5 +18,5 @@ func _on_timer_timeout() -> void:
 
 func _on_area_entered(area: Area3D) -> void:
 	if area.is_in_group("enemy_area"):
-		area.get_parent().current_health -= 25
+		area.get_parent().current_health -= damage
 		queue_free()
